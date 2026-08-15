1db44777c215258374ff2378f76d5d24a503b6d8
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 12:12:35 -0400

    update

--- src/pmscreen.py
+++ src/pmscreen.py
@@ -20,7 +20,7 @@ class PMScreenConfig:
 
 
 class PMScreen:
-    def __init__(self, _screen):
+    def __init__(self, _screen: PMScreenConfig):
         self._screen = _screen = PMScreenConfig.from_dict(_config.__dict__)
         if self._screen.rotate:
             if self._screen.rotate not in [0, 90, 180, 270]:
