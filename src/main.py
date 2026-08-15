9af34531a2a039c123fa2f9e046374f181527562
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 12:28:45 -0400

    update

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ class Main:
     def __init__(self):
         print("Initializing Main class")
         screen = PMScreen(PMScreenConfig())
-        sel
+        screen.clear()  # Clear the screen
+        screen.
 
 if __name__ == "__main__":
     main = Main()
