a68a585f7b5502e3dbd659dce5b45b765d2593ca
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 12:40:58 -0400

    update

--- src/pmscreen.py
+++ src/pmscreen.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from PIL import Image, ImageDraw
 
 DEBUG = True
+FAST_FLUSH = True
 
 def _debug(*args, **kwargs) -> None:
     """Print debug messages to the console."""
