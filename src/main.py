515c427e13278b6e41d82c1f92dd2359003106ce
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 12:36:19 -0400

    update

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ class Main:
         screen = PMScreen(PMScreenConfig())
         screen.clear()  # Clear the screen
         screen.line((0, 0, 100, 100), color=0xFF, width=2)
+        
 
 if __name__ == "__main__":
     main = Main()
