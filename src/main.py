b6091ce8b7336a5f51aa891a502779a7015209e6
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 13:15:18 -0400

    update

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ class Main:
         screen.rectangle((0, 0, 1919, 1079), color="red", width=5)  # Draw a white rectangle around the screen
         screen.line((0, 0, 1919, 1079), color="green", width=3)  # Draw a green diagonal line
         screen.line((0, 1079, 1919, 0), color="blue", width=3)  # Draw a blue diagonal line
+        screen.text((100, 100), "Hello, PMScreen!", color="yellow")  # Draw text on the screen
         screen.flush()  # Flush the changes to the framebuffer
 
     def _args(self):
