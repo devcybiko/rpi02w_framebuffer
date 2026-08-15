ef04a3a18f6f493a9623c052cfd152f7a9a0c308
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 15:45:25 -0400

    update

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ class Main:
         screen.rectangle((10, 10, 1910, 1070), color="red", width=5)  # Draw a white rectangle around the screen
         screen.line((10, 10, 1910, 1070), color="green", width=3)  # Draw a green diagonal line
         screen.line((10, 1070, 1910, 10), color="blue", width=3)  # Draw a blue diagonal line
+        
         screen.flush()  # Flush the changes to the framebuffer
 
 if __name__ == "__main__":
