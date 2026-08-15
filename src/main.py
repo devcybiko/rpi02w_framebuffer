d7cbd50b90ca02b5ec955e94c31d4ee6e822d1cd
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 15:24:41 -0400

    update

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ class Main:
     def run(self):
         print("Running Main class")
         screen = PMScreen(PMScreenConfig(), debug = not self.args.quiet)
-        screen._hard_clear(b"\x00\x07")  # Clear the framebuffer to black
+        screen._hard_clear(b"\x00\x1f")  # Clear the framebuffer to black
 
 if __name__ == "__main__":
     main = Main()
