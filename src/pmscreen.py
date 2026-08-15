0796ab10620d054d38d3afb435ec513542f3e68e
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 13:00:56 -0400

    update

--- src/pmscreen.py
+++ src/pmscreen.py
@@ -18,9 +18,9 @@ class PMScreen:
         self._fast_flush = fast_flush
         self._hard_clear(b"\x00\x00")  # Clear the framebuffer with white color
 
-    def _debug(*args, **kwargs) -> None:
+    def _debug(self, *args, **kwargs) -> None:
         """Print debug messages to the console."""
-        if DEBUG:
+        if self._debug:
             print("[DEBUG]", *args, **kwargs)
 
     def _hard_clear(self, color: bytes = b"\x00\x00") -> None:
@@ -33,10 +33,10 @@ class PMScreen:
         """Write the image to the framebuffer."""
         # self._screen.frame_buffer = "./fb0.jpg"
         if self._screen.frame_buffer:
-            _debug(f"Writing to framebuffer: {self._screen.frame_buffer}")
+            self._debug(f"Writing to framebuffer: {self._screen.frame_buffer}")
             from clib import rgba_to_rgb16
             raw = img.tobytes("raw")
-            _debug(f"Raw image size: {len(raw)} bytes")
+            self._debug(f"Raw image size: {len(raw)} bytes")
             # Convert the image to RGB565 format
             rgb565 = rgba_to_rgb16(raw, img.width, img.height)
             _debug(f"Converted to RGB565 size: {len(rgb565)} bytes")
