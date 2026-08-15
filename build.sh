d3e368c0d98e1116044478261eded7d515138668
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 15:41:04 -0400

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-# cd src/clib
-# python setup.py build_ext --inplace
-# cd ../..
+cd src/clib
+python setup.py build_ext --inplace
+cd ../..
