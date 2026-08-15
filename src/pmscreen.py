cdb1a70a1f77e47a86237b5b3301c8349d749af1
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 12:17:28 -0400

    update

--- src/pmscreen.py
+++ src/pmscreen.py
@@ -27,7 +27,6 @@ class PMScreen:
     def __init__(self, _screen: PMScreenConfig):
         self._screen = _screen or PMScreenConfig()  # Use provided config or default
         self._img = Image.new("RGBA", (self._rect.width, self._rect.height), 0)
-
         self._hard_clear()
 
     def _hard_clear(self, color: bytes = b"\x00") -> None:
