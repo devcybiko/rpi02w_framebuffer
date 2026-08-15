016afb00bf376e0e19bd14f0cdcfffe2191c71ea
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 13:27:31 -0400

    update

--- src/main.py
+++ src/main.py
@@ -15,9 +15,13 @@ class Main:
         screen.clear()  # Clear the screen
         screen.flush()  # Flush the changes to the framebuffer
         screen.rectangle((10, 10, 1910, 1070), color="red", width=5)  # Draw a white rectangle around the screen
+        screen.flush()  # Flush the changes to the framebuffer
         screen.line((10, 10, 1910, 1070), color="green", width=3)  # Draw a green diagonal line
+        screen.flush()  # Flush the changes to the framebuffer
         screen.line((10, 1070, 1910, 10), color="blue", width=3)  # Draw a blue diagonal line
+        screen.flush()  # Flush the changes to the framebuffer
         screen.text((1920//2, 1080//2), "Hello, PMScreen!", color="yellow")  # Draw text on the screen
+        screen.flush()  # Flush the changes to the framebuffer
 
     def _args(self):
         import argparse
