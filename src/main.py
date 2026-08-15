8f4a7ca9f3eb412742b88c41e82c3d57590a5d0e
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 15:36:10 -0400

    update

--- src/main.py
+++ src/main.py
@@ -19,6 +19,16 @@ class Main:
         print("Running Main class")
         screen = PMScreen(PMScreenConfig(), debug = not self.args.quiet)
         screen._hard_clear(b"\xff\xff")  # Clear the framebuffer to white
+        screen = PMScreen(PMScreenConfig(), fast_flush=args.fast, debug=not args.quiet)
+        screen.clear()  # Clear the screen
+        screen.flush()  # Flush the changes to the framebuffer
+        screen.rectangle((10, 10, 1910, 1070), color="red", width=5)  # Draw a white rectangle around the screen
+        screen.flush()  # Flush the changes to the framebuffer
+        screen.line((10, 10, 1910, 1070), color="green", width=3)  # Draw a green diagonal line
+        screen.flush()  # Flush the changes to the framebuffer
+        screen.line((10, 1070, 1910, 10), color="blue", width=3)  # Draw a blue diagonal line
+        screen.flush()  # Flush the changes to the framebuffer
+
         screen.flush()  # Flush the cleared image to the framebuffer
 
 if __name__ == "__main__":
