3c5c06a75aece17c2c450260734555022b7b0513
Gregory Smith <greg@agilefrontiers.com>
2026-08-15 14:58:46 -0400

    update

--- src/clib/__init__.py
+++
@@ -1,4 +0,0 @@
-from .clib import (
-    rgba_to_rgb16,
-    rgb_to_rgb16
-)
--- src/main.py
+++ src/main.py
@@ -1,27 +1,8 @@
-from src.pmscreen import PMScreen, PMScreenConfig
-
-DEBUG = True
-def _debug(*args, **kwargs) -> None:
-    """Print debug messages to the console."""
-    if DEBUG:
-        print("[DEBUG]", *args, **kwargs)
-
 class Main:
     def __init__(self):
         print("Initializing Main class")
         args = self._args()
         print(f"Arguments: {args}")
-        screen = PMScreen(PMScreenConfig(), fast_flush=args.fast, debug=not args.quiet)
-        screen.clear()  # Clear the screen
-        screen.flush()  # Flush the changes to the framebuffer
-        screen.rectangle((10, 10, 1910, 1070), color="red", width=5)  # Draw a white rectangle around the screen
-        screen.flush()  # Flush the changes to the framebuffer
-        screen.line((10, 10, 1910, 1070), color="green", width=3)  # Draw a green diagonal line
-        screen.flush()  # Flush the changes to the framebuffer
-        screen.line((10, 1070, 1910, 10), color="blue", width=3)  # Draw a blue diagonal line
-        screen.flush()  # Flush the changes to the framebuffer
-        screen.text((1920//2, 1080//2), "Hello, PMScreen!", color="yellow")  # Draw text on the screen
-        screen.flush()  # Flush the changes to the framebuffer
 
     def _args(self):
         import argparse
